dbfa791913a415269eacf4e79aee2ffd507b9b19
Maik-Biazi <71233006+Maik-Biazi@users.noreply.github.com>
2021-10-23 20:28:50 -0300

    engine

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,72 @@ const routes = express.Router()
 const views = __dirname + "/views/"
 
 const profile = {
-name: "Maik",
-avatar: "https://avatars.githubusercontent.com/u/73872086?v=4",
-"monthly-budget":3000,
-"days-per-week": 5,
-"hours-per-day": 5,
-"vacation-per-year": 4
+    name: "Maik",
+    avatar: "https://avatars.githubusercontent.com/u/73872086?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
 }
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        'total-hours': 60,
+        created_at: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3,
+        'total-hours': 47,
+        created_at: Date.now()
+    }
+
+]
 
 
-routes.get('/', (req,res)=> res.render( views +"index"))
-routes.get('/job', (req,res)=> res.render( views +"job"))
-routes.get('/job/edit', (req,res)=> res.render(views + "job-edit"))
-routes.get('/profile', (req,res)=> res.render(views + "profile",{profile}))
 
 
+
+
+routes.get('/', (req, res) => {
+    const updatedJobs = jobs.map(() => {
+        //ajustes no jobs
+        //calculo de tempo restante
+        const remainingDays = (jobs["total-hours"] / job
+        ["daily-hours"]).toFixed()
+        const createdDate = new Date(job.created_at)
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        // const dueDate = createdDate.setDate
+
+
+        return job
+    })
+
+    return res.render(views + "index", { jobs })
+
+})
+
+
+
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+    const lastId = jobs[jobs.length - 1]?.id || 1
+
+
+    jobs.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        'total-hours': req.body["total-hours"],
+        created_at: Date.now()
+    })
+    return res.redirect('/')
+})
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 
 module.exports = routes
